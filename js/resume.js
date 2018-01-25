2314461f7423f4d9911bd375af25d9222c4c8c4c
Anthony Pinzone <pinzone@Anthonys-MacBook-Pro.local>
2018-01-24 20:26:33 -0500

    Content implementation. Addition of Ekko Lightbox. Removed unused vendor files.

--- js/resume.js
+++ js/resume.js
@@ -25,4 +25,10 @@
     target: '#sideNav'
   });
 
+  // Ekko Lightbox using Bootstrap Modal
+  $(document).on('click', '[data-toggle="lightbox"]', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+  });
+
 })(jQuery); // End of use strict
