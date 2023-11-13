ce7a32baa8af95434b87bdad19d04aff74ee5eaa
Anthony Pinzone <anthony.pinzone@nintex.com>
2023-11-12 22:50:11 -0500

    Added the Experience section.

---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { DataContext } from "../../common/contexts";
+import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
+import "./Experience.scss";
+
+export function Experience() {
+  const data = React.useContext(DataContext);
+  if (!data) return console.error("No data was provided.");
+
+  return (
+    <section className='experience'>
+      <div className='container experience__container'>
+        <header className='experience__header'>
+          <h2 className='experience__title'>Where I've Been</h2>
+          <div className='experience__timeline-controls'>
+            <button className='experience__button'>
+              <AiOutlineLeft /> Prev
+            </button>
+            <button className='experience__button'>
+              Next <AiOutlineRight />
+            </button>
+          </div>
+        </header>
+        <div className='experience__timeline' tabIndex={-1}>
+          {data.experience?.map(
+            ({
+              company,
+              position,
+              startDate,
+              endDate,
+              skills,
+              description,
+            }) => (
+              <article
+                className='experience__timeline-article'
+                key={`${company}-${position}-${startDate}`}
+                tabIndex={0}
+              >
+                <header className='experience__timeline-header'>
+                  <h3 className='experience__timeline-company'>{company}</h3>
+                  <h4 className='experience__timeline-position'>{position}</h4>
+                  <span className='experience__timeline-date'>
+                    {startDate} - {endDate}
+                  </span>
+                </header>
+                <div className='experience__timeline-content'>
+                  <p className='experience__timeline-description'>
+                    {description}
+                  </p>
+                </div>
+              </article>
+            )
+          )}
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { DarkModeToggle } from "./DarkModeToggle/DarkModeToggle";
 export { Intro } from "./Intro/Intro";
 export { Work } from "./Work/Work";
+export { Experience } from "./Experience/Experience";
