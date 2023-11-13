74b7da903b0ea6c913419341b1ad9e9a3c878b31
Anthony Pinzone <anthony.pinzone@nintex.com>
2023-11-12 22:50:26 -0500

    Built out the homepage.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
 import { useEffect, useState } from "react";
+import { ThemeContext, DataContext } from "./common/contexts";
+import { PageLayout as Layout } from "./layout";
+import { Intro, Work, Experience } from "./components";
+import "./App.scss";
+
 function App() {
+  const [data, setData] = useState(null);
+  const [theme, setTheme] = useState("light");
+
+  const getData = async () => {
+    try {
+      const response = await fetch("data.json");
+      const data = await response.json();
+      setData(data);
+    } catch (error) {
+      console.log("There was an issue fetching the data.", error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <>
+      <DataContext.Provider value={data}>
+        <ThemeContext.Provider value={{ theme, setTheme }}>
+          <Layout>
+            <Intro />
+            <Work />
+            <Experience />
+          </Layout>
+        </ThemeContext.Provider>
+      </DataContext.Provider>
     </>
   );
 }
