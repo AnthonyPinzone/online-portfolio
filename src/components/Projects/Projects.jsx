6c528478bb480f446b38d70dbbd1fbad90604b7c
Anthony Pinzone <the1andonlypz@gmail.com>
2023-11-24 17:51:08 -0500

    Projects cleaned up to use the SkillIcon component.

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,15 +1,7 @@
 import React from 'react';
 import { DataContext } from '../../common/contexts';
 import { Container } from '../../layout';
-import { AiFillHtml5 } from 'react-icons/ai';
-import {
-  BiLogoCss3,
-  BiLogoJavascript,
-  BiLogoJquery,
-  BiLogoReact,
-  BiLogoSass,
-  BiLogoTypescript,
-} from 'react-icons/bi';
+import { SkillIcon } from '../';
 import { BsArrowUpRight } from 'react-icons/bs';
 import './Projects.scss';
 
@@ -39,46 +31,13 @@ export function Projects() {
                   </p>
                   <footer className="projects__gallery-card-footer">
                     <div className="projects__gallery-card-skills">
-                      {skills.includes('html') && (
-                        <AiFillHtml5
-                          title="HTML"
-                          fill="var(--html-logo-color)"
+                      {skills.map((skill) => (
+                        <SkillIcon
+                          className="projects__gallery-card-skill"
+                          key={skill}
+                          skill={skill}
                         />
-                      )}
-                      {skills.includes('css') && (
-                        <BiLogoCss3 title="CSS" fill="var(--css-logo-color)" />
-                      )}
-                      {skills.includes('sass') && (
-                        <BiLogoSass
-                          title="Sass"
-                          fill="var(--sass-logo-color)"
-                        />
-                      )}
-                      {skills.includes('javascript') && (
-                        <BiLogoJavascript
-                          title="Javascript"
-                          fill="var(--javascript-logo-color)"
-                        />
-                      )}
-                      {skills.includes('jquery') && (
-                        <BiLogoJquery
-                          title="jQuery"
-                          fill="var(--jquery-logo-color)"
-                        />
-                      )}
-                      {skills.includes('react') ||
-                        (skills.includes('react native') && (
-                          <BiLogoReact
-                            title="React"
-                            fill="var(--react-logo-color)"
-                          />
-                        ))}
-                      {skills.includes('typescript') && (
-                        <BiLogoTypescript
-                          title="Typescript"
-                          fill="var(--typescript-logo-color)"
-                        />
-                      )}
+                      ))}
                     </div>
                     <a className="projects__gallery-card-link" href={url}>
                       View Project <BsArrowUpRight />
