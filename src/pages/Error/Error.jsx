84447b648061e326b221abdd2c856d010923e577
Anthony Pinzone <anthonypinzone@pursuit.org>
2024-01-08 22:41:53 -0500

    Setup a generic error component to handle navigation errors.

---
+++ src/pages/Error/Error.jsx
@@ -0,0 +1,22 @@
+import { useRouteError } from "react-router-dom";
+import "./Error.scss";
+
+export default function Error() {
+    const error = useRouteError();
+    // console.log(error);
+
+    return (
+        <div className="error-page">
+            <div className="container">
+                <h1>Oops!</h1>
+                <p>Sorry, an unexpected error has occurred.</p>
+                <p>
+                    <i>{error.data || error.statusText || error.message}</i>
+                </p>
+                <p>
+                    <a href="/">Return to the home page</a>
+                </p>
+            </div>
+        </div>
+    )
+}
