110a7b920fad21a202b9f1242a3139ad421229d5
Anthony Pinzone <anthonypinzone@pursuit.org>
2024-01-08 22:39:37 -0500

    Update page layout to flex fill vertical space with content section.

--- src/layout/PageLayout/PageLayout.jsx
+++ src/layout/PageLayout/PageLayout.jsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import Header from '../PageHeader/PageHeader';
 import Footer from '../PageFooter/PageFooter';
+import './PageLayout.scss';
 
 export function PageLayout({ children }) {
   return (
