7ffd7a99c981ec44de5fbb64e880471c1215d153
Anthony Pinzone <anthonypinzone@pursuit.org>
2024-01-08 22:42:50 -0500

    Context cleanup.

--- src/common/contexts.js
+++ src/common/contexts.js
@@ -1,5 +1,4 @@
 import { createContext } from 'react';
 
 export const ThemeContext = createContext('light');
-export const DataContext = createContext([]);
 export const PrefersReducedMotionContext = createContext(false);
