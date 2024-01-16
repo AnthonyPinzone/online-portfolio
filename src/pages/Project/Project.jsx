c3554f6e08d58aaab5e8ee3ea66c7d00d2cec60e
Anthony Pinzone <anthonypinzone@pursuit.org>
2024-01-16 13:49:08 -0500

    Started working out the data and structure for the project details page.

--- src/pages/Project/Project.jsx
+++ src/pages/Project/Project.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { Container } from '../../layout';
 import ProjectNotFound from './ProjectNotFound';
 import useProjects from '../../hooks/useProjects';
+import { Hero } from '../../components';
+import './Project.scss';
 
 export default function Project() {
     const { projectSlug } = useParams();
@@ -15,8 +18,29 @@ export default function Project() {
 
     return (
         <div className="project-page">
-            <h1>{project.name}</h1>
-            <p>{project.hook}</p>
+            <Hero image={{ src: project.cover }}>
+                <h1 className="project-page__title">{project.name}</h1>
+                <p className="project-page__desc">{project.description}</p>
+            </Hero>
+
+            <Container className="project-page__container">
+                {project.features.map((feature, index) => (
+                    <section className="project-feature" key={index}>
+                        <figure className="project-feature__figure">
+                            <img src={feature.image} alt={feature.title} />
+                        </figure>
+                        <div className="project-feature__content">
+                            <h2>{feature.title}</h2>
+                            <p>{feature.description}</p>
+                            <ul className="project-feature__insights">
+                                {feature.insights.map((insight, index) => (
+                                    <li key={index}>{insight}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    </section>
+                ))}
+            </Container>
         </div>
     )
 }
